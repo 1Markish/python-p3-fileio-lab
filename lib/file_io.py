e7a809a6b39592f83e19f30acec0a4e8f1fbf2aa
1Markish <mark.kipchumba@student.moringaschool.com>
2023-08-25 03:55:01 +0300

    Done with assignment

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,17 @@
 def write_file(file_name, file_content):
-    pass
+    with open (f'{file_name}.txt', mode='w', encoding='utf-8' ) as file_name :
+        file_name.write(file_content)
+        
+write_file(file_name="logfile" ,file_content="This is a test content.")
 
 def append_file(file_name, append_content):
-    pass
+    with open(f'{file_name}.txt' , mode='a' , encoding='utf-8' ) as file_name :
+        file_name.write(append_content)
+append_file(file_name='logfile' , append_content='This is an Appended content.')
 
 def read_file(file_name):
-    pass
+    with open (f'{file_name}.txt' ,  encoding='utf-8') as test_file :
+      content= test_file.read()
+      return content 
+file_content = read_file(file_name="logfile")
+print(file_content)
